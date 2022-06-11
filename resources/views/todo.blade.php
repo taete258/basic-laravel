398f9d5963b9310045940aa4bf9d24437eadd5c7
Ratchanon Pheungta <tae.taew42@gmail.com>
2022-06-11 12:34:02 +0700

    edit database to app

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,10 +1,10 @@
 <style>
-
-    .taskName:only-of-type .delete{
-        display:none;
+    .taskName:only-of-type .delete {
+        display: none;
     }
-    .taskName:last-of-type:not(:only-of-type){
-        display:flex;
+
+    .taskName:last-of-type:not(:only-of-type) {
+        display: flex;
     }
 </style>
 <x-app-layout>
@@ -25,7 +25,7 @@
     <div class="py-4 px-6 invisible" id="table-content">
         <div class="w-full">
             <div class="shadow-md overflow-auto border-1 border-gray-200 rounded-lg bg-white">
-                <table class="w-full text-sm divide-gray-200" id="dataTable"  style="width: 100%;">
+                <table class="w-full text-sm divide-gray-200" id="dataTable" style="width: 100%;">
                     <thead>
                         <tr>
                             <th class="px-6 py-2 text-left text-md font-large text-indigo-700 ">No.</th>
@@ -50,22 +50,23 @@
             <x-slot name="content">
                 <div>
                     <x-jet-label for="toDoName" value="{{ __('To do Name') }}" />
-                    <x-jet-input id="toDoName" name="name" type="text" class="mt-1 block w-full" autofocus autocomplete="off"
-                        autocorrect="off" autocapitalize="off" spellcheck="false" />
+                    <x-jet-input id="toDoName" name="name" type="text" class="mt-1 block w-full" autofocus
+                        autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
                     <span class="text-red-500 mt-2" id="toDoNameError"></span>
                 </div>
                 <div class="mt-3">
                     <x-jet-label for="description" value="{{ __('Description') }}" />
-                    <x-jet-textarea id="description" name="description" type="text" class="mt-1 block w-full h-[70px]" autofocus
-                        autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
+                    <x-jet-textarea id="description" name="description" type="text" class="mt-1 block w-full h-[70px]"
+                        autofocus autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
                     <span class="text-red-500 mt-2" id="descriptionError"></span>
                 </div>
 
                 <div class="mt-3 task-input-group">
-                    <x-jet-label for="tasks" id="task-label" value="{{__('Tasks')}}" ></x-jet-label>
-                      <a href="#" class="float-right add_form_field text-gray-700 text-sm hover:text-indigo-500" type="button" onclick="addMoreTask(event)">Add New Field &nbsp; 
+                    <x-jet-label for="tasks" id="task-label" value="{{ __('Tasks') }}"></x-jet-label>
+                    <a href="#" class="float-right add_form_field text-gray-700 text-sm hover:text-indigo-500"
+                        type="button" onclick="addMoreTask(event)">Add New Field &nbsp;
                         <i class="fa-solid fa-plus"></i>
-                      </a>
+                    </a>
             </x-slot>
 
             <x-slot name="footer">
@@ -84,47 +85,58 @@
 <script>
     $(document).ready(function() {
         var table = getDataTableToDo();
-        $('#dataTable tbody').on( 'click', '.confirm-delete', function (e) {
-            var data = $('#dataTable').DataTable().row( $(this).parents('tr') ).data();
-            deleteToDoById(e,data)
+        console.log(table)
+        $('#dataTable tbody').on('click', '.confirm-delete', function(e) {
+            var data = $('#dataTable').DataTable().row($(this).parents('tr')).data();
+            deleteToDoById(e, data)
         });
-    });  
+    });
 </script>
 
 <script>
+    var max_fields = 10;
+    var x = 0;
 
-var max_fields = 10;
-var x = 0;
-
-    function getDataTableToDo(){
-       return $('#dataTable').dataTable({
+    function getDataTableToDo() {
+        return $('#dataTable').dataTable({
             "initComplete": function(settings, json) {
                 $('#table-content').removeClass('invisible');
                 $('#loading-screen').addClass('invisible');
             },
             ajax: `{{ url('/todo/') }}`,
-            columns: [
-                { data: "No.",
-                    render: function(data,type, rowData, meta){
+            columns: [{
+                    data: "No.",
+                    render: function(data, type, rowData, meta) {
                         return (meta.row + 1);
                     }
                 },
-                { data: "name" },
-                { data: "description" ,
-                    render: function(data,type, rowData, meta){
+                {
+                    data: "name"
+                },
+                {
+                    data: "description",
+                    render: function(data, type, rowData, meta) {
                         return (rowData.description ?? '-');
                     }
                 },
-                { data: "state",
-                    render: function(data,type, rowData, meta){
-                        return  '<span class="' + (rowData.state == 'Archived' ? 'bg-green-600' : 'bg-red-600')+  ' text-white text-md inline-flex items-center justify-center px-3 py-1 leading-none rounded-full">'+ rowData.state +'</span>';
+                {
+                    data: "state",
+                    render: function(data, type, rowData, meta) {
+                        return '<span class="' + (rowData.state == 'Archived' ? 'bg-green-600' :
+                                'bg-red-600') +
+                            ' text-white text-md inline-flex items-center justify-center px-3 py-1 leading-none rounded-full">' +
+                            rowData.state + '</span>';
                     }
                 },
-                { data: "Proceed",
-                    render: function(data,type, rowData, meta){
-                        let button1 = '<button type="button" class="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-400 inline-flex items-center px-4 py-2 bg-indigo-600 border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-400 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"><i class="fa-solid fa-list-check"></i></button>';
-                        let button2 = '<button type="button" data-name="'+rowData.name+'" data-id="'+rowData.id+'" class="bg-red-600 hover:bg-red-400 active:bg-red-400 confirm-delete inline-flex items-center px-4 py-2 bg-indigo-600 border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-400 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"> <i class="fa-solid fa-trash-can"></i></button>';
-                        return button1+button2;
+                {
+                    data: "Proceed",
+                    render: function(data, type, rowData, meta) {
+                        let button1 =
+                            '<button type="button" class="bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-400 inline-flex items-center px-4 py-2 bg-indigo-600 border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-400 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"><i class="fa-solid fa-list-check"></i></button>';
+                        let button2 = '<button type="button" data-name="' + rowData.name +
+                            '" data-id="' + rowData.id +
+                            '" class="bg-red-600 hover:bg-red-400 active:bg-red-400 confirm-delete inline-flex items-center px-4 py-2 bg-indigo-600 border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 active:bg-indigo-400 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"> <i class="fa-solid fa-trash-can"></i></button>';
+                        return button1 + button2;
                     }
                 }
             ],
@@ -135,10 +147,11 @@ var x = 0;
         e.preventDefault();
         var data = [];
         $('.taskName input[name="tasksData"]').each(function() {
-                if($(this).val() != ''){
-                    data.push({
+            if ($(this).val() != '') {
+                data.push({
                     name: $(this).val(),
-                    description: ($(this).nextAll('[name="tasksDescription"]').val() != '') ? $(this).nextAll('[name="tasksDescription"]').val(): null
+                    description: ($(this).nextAll('[name="tasksDescription"]').val() != '') ? $(this)
+                        .nextAll('[name="tasksDescription"]').val() : null
                 });
             }
         });
@@ -147,7 +160,7 @@ var x = 0;
             description: $("#description").val(),
             tasksData: data
         };
-        
+
         $.ajax({
             url: "/todo/add",
             type: "POST",
@@ -172,7 +185,7 @@ var x = 0;
         });
     };
 
-    function deleteToDoById(e,data) {
+    function deleteToDoById(e, data) {
         var name = data.name;
         var id = data.id;
         e.preventDefault();
@@ -224,11 +237,11 @@ var x = 0;
         removeAllTasks();
     }
 
-    function addMoreTask(){
+    function addMoreTask() {
         var wrapper = $(".task-input-group");
-            if (x < max_fields) {
-                x++;
-                let temp = `
+        if (x < max_fields) {
+            x++;
+            let temp = `
                 <div class="taskName flex mt-2 mb-2 w-full">
                     <div class="w-11/12">
                         <label class="text-sm text-gray-500 w-full">Task</label>
@@ -243,10 +256,10 @@ var x = 0;
                         </x-jet-danger-button>  
                     </div>
                 </div>`;
-                $(wrapper).append(temp);
-            } else {
-                alert('You Reached the task limits')
-            }
+            $(wrapper).append(temp);
+        } else {
+            alert('You Reached the task limits')
+        }
 
 
         $(wrapper).on("click", ".delete", function() {
@@ -257,9 +270,7 @@ var x = 0;
         return false;
     }
 
-    function removeAllTasks(){
+    function removeAllTasks() {
         $(".taskName").remove();
     }
 </script>
-
-
